86b9fcb49f01072e4827f6b455b207598edefd0c
PHANNA11 <phaphanna2018@gmail.com>
2023-06-19 19:30:28 +0700

    IOS

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,6 +1,9 @@
 import 'package:fetch_api/controller/product_controller.dart';
 import 'package:fetch_api/model/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:readmore/readmore.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -41,7 +44,13 @@ class _HomePageState extends State<HomePage> {
 
   Widget buildCardProduct(ProductModel product) {
     return ExpansionTile(
+      initiallyExpanded: true,
       title: Text(product.title),
+      trailing: IconButton(
+          onPressed: () async {
+            Share.share(product.image, subject: 'Look what I made!');
+          },
+          icon: Icon(Icons.share)),
       children: [
         Row(
           children: [
@@ -54,9 +63,55 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Expanded(child: Text(product.description))
+            Expanded(
+              child: ReadMoreText(
+                product.description,
+                trimLines: 6,
+                colorClickableText: Colors.pink,
+                trimMode: TrimMode.Line,
+                trimCollapsedText: 'Show more',
+                trimExpandedText: 'Show less',
+                lessStyle:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                moreStyle:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            )
           ],
-        )
+        ),
+        Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.price.toString()),
+            )),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RatingBarIndicator(
+                  rating: product.rating.rate,
+                  itemBuilder: (context, index) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  itemCount: 5,
+                  itemSize: 20.0,
+                  direction: Axis.horizontal,
+                ),
+                Text(
+                  product.rating.count.toString(),
+                ),
+                Text(
+                  product.category.name,
+                )
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
